4e45f897bc14fc98b67dda5a214a44d98a538e18
Sean <williams.sean2025@gmail.com>
2026-07-31 18:41:31 -0500

    fifth commit

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let home = document.getElementById('home');
 let explore = document.getElementById('explore');
 let now = document.getElementById('now');
 let future = document.getElementById('future');
+let coffee = document.getElementById('first');
+let coffeeReplace = document.getElementById('second');
+let coffeeEmpty = document.getElementById('third');
 
 let homeLink = document.getElementById('home-link');
 let exploreLink = document.getElementById('explore-link');
@@ -52,6 +55,28 @@ futureLink.onclick = function(event) {
     explore.classList.add('hidden');
     now.classList.add('hidden');
     future.classList.add('hidden');
+    coffee.style.display = 'none';
 
     future.classList.remove('hidden');
+};
+
+coffee.onclick = function(event) {
+    event.preventDefault();
+
+    coffee.style.display = 'none';
+    coffeeReplace.style.display = 'flex';
+};
+
+coffeeReplace.onclick = function(event) {
+    event.preventDefault();
+
+    coffeeReplace.style.display = 'none';
+    coffeeEmpty.style.display = 'flex';
+};
+
+coffeeEmpty.onclick = function(event) {
+    event.preventDefault();
+
+    coffeeEmpty.style.display = 'none';
+    coffee.style.display = 'flex';
 };
